972d019e735c7f459169d13e900b4d9a5b1be29b
kevincrafts <kevin.crafts@colorado.edu>
2018-08-31 12:36:37 -0600

    Update brand-bar.js

--- js/brand-bar.js
+++ js/brand-bar.js
@@ -1,5 +1,8 @@
 var scriptPram = document.getElementById('cu-brand-bar-embed');
 var color = scriptPram.getAttribute('data-color');
+if(color=='') {
+  color = 'black';
+}
 
 var brandBar = '<div class="brand-bar brand-bar-color-' + color + '"><a href="https://www.colorado.edu">University of Colorado Boulder</a><a href="https://www.colorado.edu/search">Search</a></div>';
 document.write(brandBar);
