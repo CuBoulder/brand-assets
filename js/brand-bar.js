5924ce50d3762794d3df8f36c70d7c3ea45fe3b2
kevincrafts <kevin.crafts@colorado.edu>
2018-08-31 12:13:10 -0600

    Create brand-bar.js

---
+++ js/brand-bar.js
@@ -0,0 +1,2 @@
+var brandBar = '<div class="brand-bar"><a href="https://www.colorado.edu">University of Colorado Boulder</a><a href="https://www.colorado.edu/search">Search</a></div>';
+document.write(brandBar);
