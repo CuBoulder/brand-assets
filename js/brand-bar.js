94b44819e5d55b71218ab6587bb340d459c8b1ea
kevincrafts <kevin.crafts@colorado.edu>
2018-08-31 12:32:02 -0600

    Update brand-bar.js

--- js/brand-bar.js
+++ js/brand-bar.js
@@ -1,2 +1,5 @@
-var brandBar = '<div class="brand-bar"><a href="https://www.colorado.edu">University of Colorado Boulder</a><a href="https://www.colorado.edu/search">Search</a></div>';
+var scriptPram = document.getElementById('cu-brand-bar-embed');
+var color = scriptPram.getAttribute('data-color');
+
+var brandBar = '<div class="brand-bar brand-bar-color-' + color + '"><a href="https://www.colorado.edu">University of Colorado Boulder</a><a href="https://www.colorado.edu/search">Search</a></div>';
 document.write(brandBar);
