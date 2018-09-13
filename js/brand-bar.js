bb6bc81451dc366f34f61f93ff3897b62173c15e
kevincrafts <kevin.crafts@colorado.edu>
2018-09-13 12:29:34 -0600

    Update brand-bar.js

--- js/brand-bar.js
+++ js/brand-bar.js
@@ -15,5 +15,5 @@ else {
 document.write('<link href="//cuboulder.github.io/brand-assets/css/brand-bar.css" rel="stylesheet" type="text/css" />');
 
 
-var brandBar = '<div class="brand-bar brand-bar-' + color + '"><a href="https://www.colorado.edu"><img src="https://cuboulder.github.io/brand-assets/images/cu-boulder-logo-text-' + logoColor + '.svg" alt="University of Colorado Boulder"</a><a href="https://www.colorado.edu/search">Search</a></div>';
+var brandBar = '<div class="brand-bar brand-bar-' + color + '"><a href="https://www.colorado.edu"><img class="ucb-logo" src="https://cuboulder.github.io/brand-assets/images/cu-boulder-logo-text-' + logoColor + '.svg" alt="University of Colorado Boulder" /></a><a href="https://www.colorado.edu/search">Search</a></div>';
 document.write(brandBar);
