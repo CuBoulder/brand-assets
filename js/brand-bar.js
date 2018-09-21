7fd9a9e191163f54e680de22383d623273ea8010
kevincrafts <kevin.crafts@colorado.edu>
2018-09-21 16:26:54 -0600

    Update brand-bar.js

--- js/brand-bar.js
+++ js/brand-bar.js
@@ -15,5 +15,5 @@ else {
 document.write('<link href="//cuboulder.github.io/brand-assets/css/brand-bar.css" rel="stylesheet" type="text/css" />');
 
 
-var brandBar = '<div class="brand-bar brand-bar-' + color + '"><a href="https://www.colorado.edu" class="ucb-home-link"><img class="ucb-logo" src="https://cuboulder.github.io/brand-assets/images/cu-boulder-logo-text-' + logoColor + '.svg" alt="University of Colorado Boulder" /></a><a class="ucb-search-link" href="https://www.colorado.edu/search"><img class="ucb-search" alt="search" src="https://cuboulder.github.io/brand-assets/images/search-' + logoColor + '.svg" /></a></div>';
+var brandBar = '<div class="ucb-brand-bar ucb-brand-bar-' + color + '"><a href="https://www.colorado.edu" class="ucb-home-link"><img class="ucb-logo" src="https://cuboulder.github.io/brand-assets/images/cu-boulder-logo-text-' + logoColor + '.svg" alt="University of Colorado Boulder" /></a><a class="ucb-search-link" href="https://www.colorado.edu/search"><img class="ucb-search" alt="search" src="https://cuboulder.github.io/brand-assets/images/search-' + logoColor + '.svg" /></a></div>';
 document.write(brandBar);
