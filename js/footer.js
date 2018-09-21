4fa67704a608ea25d847d24660abf8a5f61f5e53
kevincrafts <kevin.crafts@colorado.edu>
2018-09-21 16:52:45 -0600

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,15 @@
 var scriptPram = document.getElementById('cu-footer-embed');
-var logoColor = scriptPram.getAttribute('data-color');
+var color = scriptPram.getAttribute('data-color');
+var logoColor;
+if (color == 'black') {
+  logoColor = 'black';
+}
+else if (color == 'white') {
+  logoColor = 'white';
+}
+else {
+  logoColor = 'black';
+}
 
 
 var cuFooter = '<div class="ucb-footer"><p><a href="https://www.colorado.edu"><img class="ucb-footer-be-boulder" src="https://cuboulder.github.io/brand-assets/images/be-boulder-' + logoColor + '.svg" alt="Be Boulder." /></a></p><p><a class="ucb-home-link" href="https://www.colorado.edu">University of Colorado Boulder</a></p><p>&copy; Regents of the University of Colorado</p><p class="ucb-footer-links"><a href="http://www.colorado.edu/about/privacy-statement">Privacy</a> &bull; <a href="http://www.colorado.edu/about/legal-trademarks">Legal &amp; Trademarks</a> &bull; <a href="http://www.colorado.edu/map">Campus Map</a></p></div>';
