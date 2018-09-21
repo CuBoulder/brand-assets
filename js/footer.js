cb8339ab029ebb4bdbfc56774c9a5463ed83dfbe
kevincrafts <kevin.crafts@colorado.edu>
2018-09-21 16:46:54 -0600

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,16 +1,6 @@
 var scriptPram = document.getElementById('cu-footer-embed');
-var color = scriptPram.getAttribute('data-color');
-var logoColor;
-if (color == 'black') {
-  logoColor = 'white';
-}
-else if (color == 'white') {
-  logoColor = 'black';
-}
-else {
-  color = 'black';
-  logoColor = 'white';
-}
+var logoColor = scriptPram.getAttribute('data-color');
 
-var cuFooter = '<div class="ucb-footer ucb-' + color + '"><p><a href="https://www.colorado.edu"><img class="ucb-footer-be-boulder" src="https://cuboulder.github.io/brand-assets/images/beboulder-' + logoColor + '.svg" alt="Be Boulder." /></a></p><p><a class="ucb-home-link" href="https://www.colorado.edu">University of Colorado Boulder</a></p><p>&copy; Regents of the University of Colorado</p><p class="ucb-footer-links"><a href="http://www.colorado.edu/about/privacy-statement">Privacy</a> &bull; <a href="http://www.colorado.edu/about/legal-trademarks">Legal &amp; Trademarks</a> &bull; <a href="http://www.colorado.edu/map">Campus Map</a></p></div>';
+
+var cuFooter = '<div class="ucb-footer"><p><a href="https://www.colorado.edu"><img class="ucb-footer-be-boulder" src="https://cuboulder.github.io/brand-assets/images/beboulder-' + logoColor + '.svg" alt="Be Boulder." /></a></p><p><a class="ucb-home-link" href="https://www.colorado.edu">University of Colorado Boulder</a></p><p>&copy; Regents of the University of Colorado</p><p class="ucb-footer-links"><a href="http://www.colorado.edu/about/privacy-statement">Privacy</a> &bull; <a href="http://www.colorado.edu/about/legal-trademarks">Legal &amp; Trademarks</a> &bull; <a href="http://www.colorado.edu/map">Campus Map</a></p></div>';
 document.write(cuFooter);
