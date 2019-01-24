2a568fb0dfa8b4564fb8262bf0e3310af5f812af
kevincrafts <kevin.crafts@colorado.edu>
2019-01-24 13:53:28 -0700

    updating urls to cdn.colorado.edu

--- js/footer.js
+++ js/footer.js
@@ -12,5 +12,5 @@ else {
 }
 
 
-var cuFooter = '<div class="ucb-footer"><p><a href="https://www.colorado.edu"><img class="ucb-footer-be-boulder" src="https://cuboulder.github.io/brand-assets/images/be-boulder-' + logoColor + '.svg" alt="Be Boulder." /></a></p><p><a class="ucb-home-link" href="https://www.colorado.edu">University of Colorado Boulder</a></p><p>&copy; Regents of the University of Colorado</p><p class="ucb-footer-links"><a href="http://www.colorado.edu/about/privacy-statement">Privacy</a> &bull; <a href="http://www.colorado.edu/about/legal-trademarks">Legal &amp; Trademarks</a> &bull; <a href="http://www.colorado.edu/map">Campus Map</a></p></div>';
+var cuFooter = '<div class="ucb-footer"><p><a href="https://www.colorado.edu"><img class="ucb-footer-be-boulder" src="https://cdn.colorado.edu/static/brand-assets/live/images/be-boulder-' + logoColor + '.svg" alt="Be Boulder." /></a></p><p><a class="ucb-home-link" href="https://www.colorado.edu">University of Colorado Boulder</a></p><p>&copy; Regents of the University of Colorado</p><p class="ucb-footer-links"><a href="http://www.colorado.edu/about/privacy-statement">Privacy</a> &bull; <a href="http://www.colorado.edu/about/legal-trademarks">Legal &amp; Trademarks</a> &bull; <a href="http://www.colorado.edu/map">Campus Map</a></p></div>';
 document.write(cuFooter);
