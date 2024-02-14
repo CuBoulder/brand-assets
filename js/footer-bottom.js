6cf533ce3149c925651be2c13162350784f49afb
kevincrafts <kevin.crafts@colorado.edu>
2024-02-14 14:06:50 -0700

    Adding link to footer css

--- js/footer-bottom.js
+++ js/footer-bottom.js
@@ -1,4 +1,5 @@
 // Requires brand-bar-top.js
+document.write('<link href="https://cuboulder.github.io/brand-assets/css/footer.css" rel="stylesheet" type="text/css" />');
 
 var cufooter = '<div class="ucb-footer black center"><p><a href="https://www.colorado.edu"><img class="ucb-footer-be-boulder" src="https://cdn.colorado.edu/static/brand-assets/live/images/be-boulder-white.svg" alt="Be Boulder." style="max-width:240px; height:auto;"></a></p><p><a class="ucb-home-link" href="https://www.colorado.edu">University of Colorado Boulder</a></p><p>© Regents of the University of Colorado</p><p class="ucb-footer-links"><a href="http://www.colorado.edu/about/privacy-statement">Privacy</a> • <a href="http://www.colorado.edu/about/legal-trademarks">Legal &amp; Trademarks</a> • <a href="http://www.colorado.edu/map">Campus Map</a></p></div>';
 $(document).ready(function(){
